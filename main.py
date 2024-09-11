c2f97338781be819d42f042abd6e0af511ed0f6c
r3tiu <tbp@sv-tel.ru>
2024-09-11 11:00:00 +0300

    + обработка индексов

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
 import sys, os, logging, subprocess
 from db.sybase import DbConnection
 from queries.all_addresses_code import all_addr
-from queries.geocoded_addresses import existing_addresses
+from queries.geocoded_addresses import existing_addresses, address_without_post_code, update_post_code
 from queries.insert_new_record import addr_ins
 from yandex_geocoder import Client, YandexGeocoderException, NothingFound, InvalidKey
 from settings import GeoCoders
 from decorators import log_decorator
+from icecream import ic
 
 
 logging.basicConfig(level=logging.DEBUG,
                     filename='log.txt',
                     filemode='a',
                     format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
-                    datefmt='%H:%M:%S')
+                    datefmt='%Y-%m-%d %H:%M:%S')
 
 yandex_client = Client(GeoCoders.yandex_geocoder)
 
-all_addresses_list: list[dict] = []
-geocoded_addresses: list = []
-
-exist_cnt: int = 0
-added_cnt: int = 0
-notfound_cnt: int = 0
+all_addresses_list, geocoded_addresses = [], []
+exist_cnt, added_cnt, notfound_cnt = 0, 0, 0
 
 
 def create_virtualenv():
@@ -35,7 +32,7 @@ def install_requiremets():
 
 
 def get_all_addresses() -> None:
-    """ Получение списка всех адресов по задданым в условии запроса критериям """
+    """ Получение списка всех адресов по заданным в условии запроса критериям """
     result = DbConnection.execute_query(all_addr)
     all_addresses_list.extend(result)
 
@@ -51,9 +48,31 @@ def insert_new_address(address_code: int, longitude: float, latitude: float) -> 
     DbConnection.execute_query(addr_ins, address_code, longitude, latitude)
 
 
+def get_addresses_wo_index() -> dict:
+    """ Получение списка адресов у которых отсутствует почтовый индекс """
+    address_list = DbConnection.execute_query(address_without_post_code)
+    address_dict = {}
+    # Заполним словарь результатом выборки
+    for el in address_list:
+        address_dict[int(el['ADDRESS_CODE'])] = f"{el['COUNTRY']},{el['REGION_NAME']},{el['AREA_NAME']},{el['TOWN']},{el['STREET']},{el['HOUSE']}"
+    # Удалим лишнее из строки адреса
+    for k in address_dict:
+        address_dict[k] = address_dict[k].replace('None','').lstrip(',')
+    # Вернем нормализованый словарь
+    return address_dict
+
+
+def insert_post_index(index: str, address_code: int) -> None:
+    """ Добавление почтового индекса  по address_code в базе"""
+    DbConnection.execute_query(update_post_code, index, address_code)
+
+
 @log_decorator
 def geocode() -> any:
-    global exist_cnt, added_cnt
+    global exist_cnt, added_cnt, notfound_cnt
+    all_addresses_list.clear()
+    geocoded_addresses.clear()
+    exist_cnt, added_cnt, notfound_cnt = 0, 0, 0
     somelist = []
     get_geocoded_addresses()
     get_all_addresses()
@@ -79,7 +98,6 @@ def geocode() -> any:
                     f" {el['COUNTRY_NAME']},{el['COUNTRY_REGION_NAME']},{el['AREA_REGION_NAME']},"
                     f"{el['TOWN_NAME']},{el['STREET_NAME']},{el['HOUSE']}")
             except NothingFound:
-                global notfound_cnt
                 notfound_cnt += 1
                 logging.warning(
                     f"Ничего не нашли для: {el['ADDRESS_CODE']}({el['COUNTRY_NAME']},{el['COUNTRY_REGION_NAME']},"
@@ -87,22 +105,40 @@ def geocode() -> any:
             except YandexGeocoderException as y:
                 logging.error(y)
             # finally:
-            #     logging.info(f"Геокодирование завершено")
+            #     logging.info(f"Процесс геокодирования завершен")
 
 
-    # return exist_cnt, added_cnt
+def post_index_processing() -> None:
+    normalized_addresses = get_addresses_wo_index()
+    index_dict = {}
+    for addr in normalized_addresses:
+        try:
+            index = yandex_client.index_by_address(normalized_addresses[addr])
+            index_dict[addr] = index
+        except NothingFound:
+            # logging.warning(
+            print(
+                f"Адрес не найден: ({addr}: {normalized_addresses[addr]})"
+            )
+        print(index_dict)
 
 
-async def main():
-    pass
+def main():
+    # while True:
+    #     geocode()
+    #     logging.info(f"Геокодировано: {added_cnt} , пропущено: {exist_cnt - notfound_cnt}, ошибочных адресов: {notfound_cnt}\n")
+    post_index_processing()
+        # sleep(3600)
 
 
 if __name__ == '__main__':
     if os.getenv('VIRTUAL_ENV'):
-        geocode()
-        logging.info(f"Геокодировано: {added_cnt} записей, пропущено: {exist_cnt - notfound_cnt} записей, ошибочных адресов: {notfound_cnt}")
+        # geocode()
+        main()
+        # logging.info(f"Геокодировано: {added_cnt} , пропущено: {exist_cnt - notfound_cnt}, ошибочных адресов: {notfound_cnt}")
     else:
         logging.info('Running outside venv!')
         create_virtualenv()
         install_requiremets()
-        geocode()
+        # geocode()
+        main()
--- queries/geocoded_addresses.py
+++ queries/geocoded_addresses.py
@@ -3,3 +3,26 @@ select ADDRESS_CODE
 from SV..ADDRESSES_COORD
 order by ADDRESS_CODE
 """
+
+address_without_post_code ="""
+select A.ADDRESS_CODE,
+       isnull(rtrim(C.COUNTRY_NAME),null) COUNTRY, isnull(rtrim(CR.COUNTRY_REGION_NAME),null) REGION_NAME,
+       isnull(rtrim(AR.AREA_REGION_NAME),null) AREA_NAME,
+       isnull(rtrim(T.TOWN_PREFIX)||rtrim(T.TOWN_NAME), null) TOWN,
+       rtrim(S.STREET_NAME) STREET,
+       case when A.HOUSE_POSTFIX is not null then cast(A.HOUSE as varchar(5))||' корп.'||A.HOUSE_POSTFIX else
+           cast(A.HOUSE as varchar(5)) end as HOUSE
+from INTEGRAL..ADDRESS A
+join INTEGRAL..STREETS S on A.STREET_CODE = S.STREET_CODE
+join INTEGRAL..TOWNS T on S.TOWN_CODE = T.TOWN_CODE
+left join INTEGRAL..AREA_REGIONS AR on T.AREA_REGION_CODE = AR.AREA_REGION_CODE
+left join INTEGRAL..COUNTRY_REGIONS CR on AR.COUNTRY_REGION_CODE = CR.COUNTRY_REGION_CODE
+left join INTEGRAL..COUNTRIES C on T.COUNTRY_CODE = C.COUNTRY_CODE and CR.COUNTRY_CODE = C.COUNTRY_CODE
+where POST_INDEX is null
+"""
+
+update_post_code = """
+update INTEGRAL..ADDRESS
+set POST_INDEX = ?
+where ADDRESS_CODE = ?
+"""
